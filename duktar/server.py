2e603d6a167cfa538352a3b19ff092040039fa4b
Anna Zimmer <anna.m.zimmer17@gmail.com>
2026-04-29 08:24:07 -0400

    increased corruption threshold

--- duktar/server.py
+++ duktar/server.py
@@ -58,7 +58,7 @@ _save_lock = threading.Lock()
 bent_images = {}
 
 # How many total databend passes before fully corrupted
-MAX_BEND_PASSES = 60
+MAX_BEND_PASSES = 120
 
 # Track how many passes each image has had
 bend_pass_counts = {}

3c4c14e0555359fa1a974cc40c71d00fd2bf4234
Anna Zimmer <anna.m.zimmer17@gmail.com>
2026-04-29 08:31:36 -0400

    upped max bend passes

--- duktar/server.py
+++ duktar/server.py
@@ -58,7 +58,7 @@ _save_lock = threading.Lock()
 bent_images = {}
 
 # How many total databend passes before fully corrupted
-MAX_BEND_PASSES = 120
+MAX_BEND_PASSES = 600
 
 # Track how many passes each image has had
 bend_pass_counts = {}
